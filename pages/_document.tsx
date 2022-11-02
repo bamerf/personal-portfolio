de4fb7b4b30293150cfcb078c7e33fcfbccfff44
Bamdad Erfanian <47050979+bamerf@users.noreply.github.com>
2022-11-02 19:31:35 +0300

    update link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,7 @@ export default function Document() {
     <Html>
       <Head title="Bamdad Erfanian - Senior Software Engineer">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="*"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
