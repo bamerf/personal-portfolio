5d5e5dcbcbca5b37f23f65391a978ea7b448c891
Bamdad Erfanian <47050979+bamerf@users.noreply.github.com>
2022-11-03 11:20:44 +0300

    add local font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head title="Bamdad Erfanian - Senior Software Engineer">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width"
