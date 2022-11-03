16a40202f1193d6d71162b97995eb3c754520d85
Bamdad Erfanian <47050979+bamerf@users.noreply.github.com>
2022-11-03 11:20:35 +0300

    resolve conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    'next/core-web-vitals',
     require.resolve('@bamerf/linter/eslint'),
     require.resolve('@bamerf/linter/eslint-typescript'),
     require.resolve('@bamerf/linter/eslint-react'),
