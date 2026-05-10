0c581983d4f1c78e0977ae5a007e50826415e4b5
Bamdad Erfanian <erfanianbamdad@gmail.com>
2026-05-10 11:21:37 -0400

    refactor(deps): remove linter package

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    require.resolve('@bamerf/linter/eslint'),
-    require.resolve('@bamerf/linter/eslint-typescript'),
-    require.resolve('@bamerf/linter/eslint-react'),
-    require.resolve('@bamerf/linter/eslint-next'),
-  ],
-};
